41e18abeeaa04ae4cb485f4cab110288acedf3c9
Gabriel Wachira <gabbyk.com@gmail.com>
2024-03-01 00:33:31 +0300

    refactor

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,21 +2,35 @@
 
 """Module for making change -coins."""
 
-def makeChange(coins: list, total: int) -> int:
-    """Given a pile of coins of different values,
-    determine the fewest number of coins
-    needed to meet a given amount (total)."""
+from typing import List
+
+def makeChange(coins: List[int], total: int) -> int:
+    """
+    Calculate the fewest number of coins needed to meet the given total amount.
+
+    Args:
+        coins (list[int]): List of coin values.
+        total (int): The target total amount.
+
+    Returns:
+        int: Fewest number of coins needed to meet the total.
+            If total is 0 or less, returns 0.
+            If total cannot be met by any number of coins, returns -1.
+    """
     if total <= 0:
         return 0
-    INF = float('inf')
-    dp = [INF] * (total + 1)
 
+    # Initialize an array to store the minimum number of coins for each amount
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
+    # Dynamic programming approach
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], 1 + dp[i -coin])
-
-    return dp[total] if dp[total] != INF else -1
+    # Check if the total amount can be achieved
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
